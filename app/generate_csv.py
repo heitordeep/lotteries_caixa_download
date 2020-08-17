eef841b1965ea34165ae66cde8c425e855c414cc
Fernando Heitor Lopes <heitor.aguia@gmail.com>
2020-08-17 16:58:41 -0300

    Code improvement

--- app/generate_csv.py
+++ app/generate_csv.py
@@ -1,23 +1,23 @@
 from datetime import datetime as dt
 from os import makedirs, path
-from time import sleep
 
 import pandas as pd
 from rich.console import Console
 
 from app.mega_sena import MegaSenaDownload
 
+console = Console()
+
 
 class GeneratorCsv:
     def __init__(self):
         self.date = dt.now().strftime('%Y-%m-%d')
-        self.console = Console()
 
     def convert_to_csv(self):
-        self.console.log(f'Buscando arquivo [bold cyan]d_mega.htm[/bold cyan]')
+        console.log(f'Buscando arquivo [bold cyan]d_mega.htm[/bold cyan]')
         if path.exists(f'raw/megasena/{self.date}/d_mega.htm'):
-            sleep(1)
-            self.console.log(
+
+            console.log(
                 f'Arquivo [bold cyan]d_mega.htm[/bold cyan] encontrado :smiley:'
             )
             df = pd.read_html(f'raw/megasena/{self.date}/d_mega.htm')
@@ -47,10 +47,10 @@ class GeneratorCsv:
                 'Acumulado_Mega_da_Virada': df['Acumulado_Mega_da_Virada'],
             }
 
-            self.console.log(
+            console.log(
                 f'Obtendo os dados do arquivo [bold cyan]d_mega.htm[/bold cyan]...'
             )
-            sleep(1)
+
             self.create_csv(
                 data=data,
                 path_file=f'swamp/megasena/{self.date}',
@@ -60,13 +60,11 @@ class GeneratorCsv:
     def sanitize_csv(self):
 
         path_file = f'swamp/megasena/{self.date}/nao_tratado.csv'
-        sleep(1)
-        self.console.log(
-            f'Buscando arquivo [bold cyan]nao_tratado.csv[/bold cyan]'
-        )
+
+        console.log(f'Buscando arquivo [bold cyan]nao_tratado.csv[/bold cyan]')
         if path.exists(path_file):
-            sleep(1)
-            self.console.log(
+
+            console.log(
                 f'Arquivo [bold cyan]nao_tratado.csv[/bold cyan] encontrado :smiley:'
             )
             df = pd.read_csv(f'{path_file}')
@@ -96,11 +94,9 @@ class GeneratorCsv:
                 'Acumulado_Mega_da_Virada': df['Acumulado_Mega_da_Virada'],
             }
 
-            self.console.log(
+            console.log(
                 f'Obtendo os dados do arquivo [bold cyan]nao_tratado.csv[/bold cyan]...'
             )
-
-            sleep(1)
 
             self.create_csv(
                 data=data,
@@ -114,8 +110,8 @@ class GeneratorCsv:
 
         makedirs(path_file, exist_ok=True)
         data_csv.to_csv(f'{path_file}/{source_file}', index=False)
-        sleep(1)
-        self.console.log(
+
+        console.log(
             f'Arquivo [bold cyan]{source_file}[/bold cyan] criado com sucesso! '
             f'Verifique na pasta: {path_file} '
         )
--- app/mega_sena.py
+++ app/mega_sena.py
@@ -7,53 +7,48 @@ from requests import get
 from requests.exceptions import HTTPError
 from rich.console import Console
 
+console = Console()
+
 
 class MegaSenaDownload:
     def __init__(self, source_file, url):
-        self.url = url
-        self.response = get(self.url, stream=True)
+        self.response = get(url, stream=True)
         self.date = dt.now().strftime('%Y-%m-%d')
         self.source_file = source_file
-        self.console = Console()
 
     def verification_http(self):
 
         try:
             self.response.raise_for_status()
-        except HTTPError as e:
-            raise SystemExit(f'Error: {e}')
-        except Exception as e:
-            raise SystemExit(f'Error: {e}')
-        else:
-            self.write_file()
-
-    def write_file(self):
-        try:
-
-            with open(f'{self.source_file}', "wb") as file_zip:
+            
+            # Download File 
+            with open(f'{self.source_file}', 'wb') as file_zip:
                 for data in self.response.iter_content():
                     file_zip.write(data)
-                self.console.log(
+                console.log(
                     f'Arquivo [bold cyan]{self.source_file}[/bold cyan] baixado com sucesso!'
                 )
-            sleep(1)
-            self.extract_file()
+                
+            # Extracted File
+            with ZipFile(f'{self.source_file}', 'r') as extract_file:
+                extract_file.extractall(f'raw/megasena/{self.date}')
+            console.log(
+                f'Arquivo [bold cyan]{self.source_file}[/bold cyan] extraido com sucesso! '
+                f'Verifique na pasta raw/megasena/{self.date}'
+            )
+            remove(f'{self.source_file}')
 
-        except Exception as e:
-            raise SystemExit(f'Error: {e}')
-
-    def extract_file(self):
-        try:
-
-            if path.exists(f'{self.source_file}'):
-
-                with ZipFile(f'{self.source_file}', 'r') as extract_file:
-                    extract_file.extractall(f'raw/megasena/{self.date}')
-                self.console.log(
-                    f'Arquivo [bold cyan]{self.source_file}[/bold cyan] extraido com sucesso! '
-                    f'Verifique na pasta raw/megasena/{self.date}'
+        except HTTPError as e:
+            count = 3
+            if count != 0:
+                console.log(
+                    f'{e}'
+                    f'\nAguarde [bold yellow]60[/bold yellow] segundos para tentar novamente.'
                 )
-                remove(f'{self.source_file}')
+                sleep(60)
+                self.verification_http()
+
+            raise SystemExit(f'Error: {e}')
 
         except Exception as e:
             raise SystemExit(f'Error: {e}')
